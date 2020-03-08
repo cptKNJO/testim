67e98e9304f5363f8d9e4ea13e9b6e9e6f6a727c
Kazi Noshin J. O <kazinoshin.jo@gmail.com>
2020-03-08 17:51:45 +0600

    iframe examples

---
+++ tests/iframe.test.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const expect = require('chai').expect;
+const {
+  go,
+  waitForElement,
+  withContext,
+  test,
+  text,
+  l,
+  Locator
+} = require('testim');
+
+const url = 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe';
+
+Locator.set(require('./locators/locators.js'));
+
+describe('iframe page loads component', () => {
+  beforeEach(async () => {
+    await go(url);
+  });
+
+  test('check title', async () => {
+    await waitForElement(l('<iframe>:_The_Inline_Frame_element'));
+  });
+
+  test('html demo loads', async () => {
+    await waitForElement('h4');
+    const title = await text('h4');
+    expect(title).to.equal('HTML');
+  });
+
+  test('css tab hidden', async () => {
+    const demo = withContext({
+      frameSelector: 'iframe',
+      tabUrl: '*developer.mozilla.org*',
+      tabIndex: 0
+    });
+    await demo.waitForNoElement('#css-panel');
+  });
+
+  test('show css tab on click', async () => {
+    const demo = withContext({
+      frameSelector: 'iframe',
+      tabUrl: '*developer.mozilla.org*',
+      tabIndex: 0
+    });
+    await demo.waitForElement(l('CSS'));
+    await demo.click(l('CSS'));
+    await demo.waitForElement('#css-panel');
+  });
+
+  test('hide html tab when css tab clicked', async () => {
+    const demo = withContext({
+      frameSelector: 'iframe',
+      tabUrl: '*developer.mozilla.org*',
+      tabIndex: 0
+    });
+    await demo.waitForElement('#html-panel');
+    await demo.click(l('CSS'));
+    await demo.waitForNoElement('#html-panel');
+  });
+});
